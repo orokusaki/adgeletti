a52ce0ad0cd896e78c5867958bfe782a2c2a2155
Jeff Ober <jeff.ober@coxinc.com>
2012-11-07 13:59:46 -0500

    Added docs

--- adgeletti/templatetags.py
+++ adgeletti/templatetags.py
@@ -7,12 +7,17 @@ ADS = "_adgeletti_ads"
 register = template.Library()
 
 def build_div(slot, breakpoint):
+    """Builds an empty div into which an ad is to be placed.
+    """
     # TODO escape div id
     return '<div class="adgeletti-ad-div" id="%s-%s"></div>' % (slot, breakpoint)
 
 
 @register.tag(name="ad")
 def parse_ad(parser, token):
+    """Parser for ad tag. Usage:
+        {% ad SLOT BREAKPOINT [BREAKPOINT ...] %}
+    """
     args = token.split_contents()
 
     if len(args) < 2:
@@ -24,6 +29,8 @@ def parse_ad(parser, token):
 
 
 class AdNode(template.Node):
+    """Emits a div with a unique id for each ad possible at the tag's location.
+    """
     def __init__(self, slot, breakpoints):
         self.slot = slot
         self.breakpoints = breakpoints
@@ -41,10 +48,21 @@ class AdNode(template.Node):
 
 @register.tag(name="adgeletti_go")
 def parse_adgeletti_go(parser, token):
+    """Parser for adgeletti_go tag. Usage:
+        {% adgeletti_go %}
+    """
     return AdBlock()
 
 
 class AdBlock(template.Node):
+    """Emits a Javascript lookup table assigned to Adgeletti.ad_data. The table
+    stores the following information about ads registered to the page by the ad
+    tag:
+
+        Adgeletti[ad_data][BREAKPOINT][SLOT] = { ad_unit_id: '???', sizes: [[?,?], [?,?], ...] }
+
+    ...where sizes is an array of [width, height] pairs (arrays).
+    """
     def render(self, context):
         if ADS not in context:
             return ''

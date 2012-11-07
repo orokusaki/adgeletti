8687196e5ef6e49963cd321bd6e5fd64082c8bc5
Jeff Ober <jeff.ober@coxinc.com>
2012-11-07 12:17:32 -0500

    Rejiggering of models

--- adgeletti/models.py
+++ adgeletti/models.py
@@ -1,26 +1,9 @@
 from django.db import models
 from django.contrib.sites.models import Site
 from django.contrib.sites.managers import CurrentSiteManager
+from django.conf import settings
 
-class Slot(models.Model):
-    """Defines an advertising slot that may be placed in a template.
-    """
-    label = models.CharField(max_length=25, unique=True)
-
-    def __unicode__(self):
-        return self.label
-
-
-class Breakpoint(models.Model):
-    """Defines a responsive breakpoint for which a Slot may be displayed.
-    """
-    label = models.CharField(max_length=25, unique=True)
-
-    def __unicode__(self):
-        return self.label
-
-
-class Size:
+class Size(models.Model):
     """Defines an ad size to make available for a Breakpoint.
     """
     width = models.PositiveIntegerField()
@@ -33,25 +16,22 @@ class Size:
         return '%dpx x %dpx' % (self.width, self.height)
 
 
-class AdUnit:
-    size = models.ForeignKey(Size)
-    ad_unit_id = models.CharField(max_length=255)
+class AdUnit(models.Model):
+    """Represents an ad unit and a configured size that it can support.
+    """
+    ad_unit_id = models.CharField(max_length=255, unique=True)
+    sizes = models.ManyToManyField(Size)
+    site = models.ForeignKeyField(Site)
 
-    class Meta:
-        unique_together = ('size', 'ad_unit_id')
+    objects = CurrentSiteManager()
 
 
 class Ad(models.Model):
-    """Selects the size of ad that displays in a Slot at a particular
-    Breakpoint.
+    """Selects the AdUnit to display in a Slot at a particular Breakpoint.
     """
-    sites = models.ManyToManyField(Site)
-    slot = models.ForeignKey(Slot)
-    breakpoint = models.ForeignKey(Breakpoint)
+    slot = models.CharField(max_length=50, choices=settings.ADGELETTI_SLOTS)
+    breakpoint = models.CharField(max_length=50, choices=settings.ADGETLETTI_BREAKPOINTS)
     ad_unit = models.ForeignKey(AdUnit)
 
-    objects = CurrentSiteManager()
-    all_sites = models.Manager()
-
-    def __unicode__(self):
-        return 'Ad <%s/%s/%s>' % (self.slot, self.breakpoint, self.size)
+    class Meta:
+        unique_together = ('slot', 'breakpoint', 'ad_unit')

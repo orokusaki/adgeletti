2ff1b1e811cbb29741c1ee733c5f3789a2af4f2c
Jeff Ober <jeff.ober@coxinc.com>
2012-11-07 12:23:59 -0500

    Spelling, cleanup

--- adgeletti/models.py
+++ adgeletti/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.sites.models import Site
-from django.contrib.sites.managers import CurrentSiteManager
 from django.conf import settings
 
 class Size(models.Model):
@@ -19,17 +18,18 @@ class Size(models.Model):
 class AdUnit(models.Model):
     """Represents an ad unit and a configured size that it can support.
     """
-    ad_unit_id = models.CharField(max_length=255, unique=True)
-    site = models.ForeignKeyField(Site)
+    unit_id = models.CharField(max_length=255, unique=True)
+    site = models.ForeignKey(Site)
 
-    objects = CurrentSiteManager()
+    def ad_unit_id(self):
+        return '%s/%s' % (settings.ADGELETTI_NETWORK_ID, self.unit_id)
 
 
 class AdPosition(models.Model):
     """Selects the AdUnit to display in a Slot at a particular Breakpoint.
     """
     slot = models.CharField(max_length=50, choices=settings.ADGELETTI_SLOTS)
-    breakpoint = models.CharField(max_length=50, choices=settings.ADGETLETTI_BREAKPOINTS)
+    breakpoint = models.CharField(max_length=50, choices=settings.ADGELETTI_BREAKPOINTS)
     sizes = models.ManyToManyField(Size)
     ad_unit = models.ForeignKey(AdUnit)
 

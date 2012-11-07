d64a9f468d38c62316a9e1c5b883d6dc6901458f
Jeff Ober <jeff.ober@coxinc.com>
2012-11-07 11:51:37 -0500

    Basic outline of models

---
+++ adgeletti/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.sites.models import Site
+from django.contrib.sites.managers import CurrentSiteManager
+
+class Slot(models.Model):
+    """Defines an advertising slot that may be placed in a template.
+    """
+    label = models.CharField(max_length=25, unique=True)
+
+    def __unicode__(self):
+        return self.label
+
+
+class Breakpoint(models.Model):
+    """Defines a responsive breakpoint for which a Slot may be displayed.
+    """
+    label = models.CharField(max_length=25, unique=True)
+
+    def __unicode__(self):
+        return self.label
+
+
+class Size:
+    """Defines an ad size to make available for a Breakpoint.
+    """
+    width = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('width', 'height')
+
+    def __unicode__(self):
+        return '%dpx x %dpx' % (self.width, self.height)
+
+
+class AdUnit(models.Model):
+    """Selects the size of ad that displays in a Slot at a particular
+    Breakpoint.
+    """
+    sites = models.ManyToManyField(Site)
+    slot = models.ForeignKey(Slot)
+    breakpoint = models.ForeignKey(Breakpoint)
+    size = models.ForeignKey(Size)
+    ad_unit_id = models.CharField(max_length=255)
+
+    objects = CurrentSiteManager()
+    all_sites = models.Manager()
+
+    def __unicode__(self):
+        return 'Ad <%s/%s/%s>' % (self.slot, self.breakpoint, self.size)

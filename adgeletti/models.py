25320c909a439e947c0636ff004b8f32bc263edf
Jeff Ober <jeff.ober@coxinc.com>
2012-11-07 12:29:42 -0500

    cleanup

--- adgeletti/models.py
+++ adgeletti/models.py
@@ -5,8 +5,8 @@ from django.conf import settings
 class Size(models.Model):
     """Defines an ad size to make available for a Breakpoint.
     """
-    width = models.PositiveIntegerField()
-    height = models.PositiveIntegerField()
+    width = models.PositiveIntegerField(help_text="The width of the ad in pixels.")
+    height = models.PositiveIntegerField(help_text="The height of the ad in pixels.")
 
     class Meta:
         unique_together = ('width', 'height')
@@ -18,11 +18,12 @@ class Size(models.Model):
 class AdUnit(models.Model):
     """Represents an ad unit and a configured size that it can support.
     """
-    unit_id = models.CharField(max_length=255, unique=True)
+    ad_unit = models.CharField(max_length=255, unique=True, help_text="The ad unit id, without the network ID")
     site = models.ForeignKey(Site)
 
+    @property
     def ad_unit_id(self):
-        return '%s/%s' % (settings.ADGELETTI_NETWORK_ID, self.unit_id)
+        return '%s/%s' % (settings.ADGELETTI_DFP_NETWORK_ID, self.ad_unit)
 
 
 class AdPosition(models.Model):

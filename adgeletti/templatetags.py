d98638536cf359b2aba01d2270082ef14136859d
Jeff Ober <jeff.ober@coxinc.com>
2012-11-07 14:40:29 -0500

    Added error msgs, minor improvements

--- adgeletti/templatetags.py
+++ adgeletti/templatetags.py
@@ -3,9 +3,17 @@ from django import template
 from django.utils.html import escape
 from adgeletti.models import AdPosition
 
-ADS = "_adgeletti_ads"
+ADS = '_adgeletti_ads'
+FIRED = '_adgeletti_fired'
 
 register = template.Library()
+
+
+def error(text):
+    """Formats error text as an (escaped) HTML comment.
+    """
+    return '<!-- %s -->' % escape(text)
+
 
 @register.tag(name="ad")
 def parse_ad(parser, token):
@@ -49,8 +57,12 @@ class AdNode(template.Node):
                 % (slot, breakpoint, slot, breakpoint)
 
     def render(self, context):
+        if FIRED in context and context[FIRED]:
+            return error('adgeletti_go has already been fired!')
+
         if ADS not in context:
             context[ADS] = {}
+            context[FIRED] = False
 
         if self.slot not in context[ADS]:
             context[ADS][self.slot] = []
@@ -77,27 +89,39 @@ class AdBlock(template.Node):
     ...where sizes is an array of [width, height] pairs (arrays).
     """
     def render(self, context):
-        if ADS not in context:
+        if ADS not in context or FIRED not in context:
             return ''
 
-        # Get database data
-        positions = AdPosition.for_site.filter(
-            slot_in=context[ADS].keys(),
-            breakpoint_in=(bp for bp in (context[ADS][slot] for slot in context[ADS])),
-        )
+        if context[FIRED]:
+            return error('adgeletti_go has already been fired!')
+        else:
+            context[FIRED] = True
 
         # Start building output
         parts = []
+
+        # Get database data
+        slots = context[ADS].keys()
+        breakpoints = set(bp for bp in (context[ADS][slot] for slot in context[ADS]))
+        positions = AdPosition.for_site.filter(slot_in=slots, breakpoint_in=breakpoints)
+
+        if not slots:
+            parts.append(error('No ads have been placed on the page.'))
+
+        if slots and not positions:
+            parts.append(error('No AdPositions have been added to the database.'))
+
         parts.append('<script type="text/javascript">')
         parts.append('var Adgeletti = {ad_data: {}};');
 
-        # Build lookup table
-        for pos in positions:
-            sizes = '[%s]' % ','.join(['[%d,%d]' % (s.width, s.height) for s in pos.sizes])
-            parts.append('Adgeletti.ad_data[%s][%s] = {ad_unit_id: "%s", sizes: [%s]};'
-                    % (pos.breakpoint, pos.slot, pos.ad_unit.ad_unit_id, sizes))
+        if positions:
+            # Build lookup table
+            for pos in positions:
+                sizes = '[%s]' % ','.join(['[%d,%d]' % (s.width, s.height) for s in pos.sizes])
+                parts.append('Adgeletti.ad_data[%s][%s] = {ad_unit_id: "%s", sizes: [%s]};'
+                        % (pos.breakpoint, pos.slot, pos.ad_unit.ad_unit_id, sizes))
 
-        # Output as Javascript
         parts.append('</script>')
+
         return "\n".join(parts)
 
